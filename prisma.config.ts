7c0f82503c2ce5c8ec648a3d1d8cd8a66649483d
Facundo28 <jarafacu1@gmail.com>
2025-12-27 17:03:05 -0300

    Fix Prisma config fallback DATABASE_URL

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,14 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const DATABASE_URL = process.env["DATABASE_URL"] ?? "file:./dev.db";
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: DATABASE_URL,
   },
 });
